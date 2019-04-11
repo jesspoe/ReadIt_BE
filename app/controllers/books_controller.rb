011425f8be0ceec5578a84d8b3d7c32927e46c3e
kwmcminn <kwmcminn@gmail.com>
2019-04-11 11:52:30 -0700

    Setup controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,40 @@
 class BooksController < ApplicationController
+
+   before_action :set_book, only: [:edit, :update, :show]
+
+   def index
+     @books = Book.all
+     render json: @books
+   end
+
+   def show
+   end
+
+   def new
+     @book = Book.new
+   end
+
+   def create
+    @book = Book.create(book_params)
+   render json: @book
+  end
+
+   def edit
+   end
+
+   def update
+     @book.update(post_params)
+     redirect_to @book
+   end
+
+
+   private
+
+     def post_params
+       params.require(:book).permit(:grade_id, :title, :img, :paragraph, :user_id)
+     end
+
+     def set_book
+       @book = Book.find(params[:id])
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :books, :grades, :words
 end
